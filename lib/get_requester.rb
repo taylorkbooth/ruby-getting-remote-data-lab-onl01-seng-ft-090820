a0702bacf8e4a40c749cbc1b7598b286ae4797f1
Taylor Booth <taylorkbooth@gmail.com>
2020-10-06 02:49:04 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,23 @@
-# Write your code here
+require 'open-uri'
+require 'net/http'
+
+
+class GetRequester
+  
+  attr_accessor :url
+  
+  def initialize(url)
+    @url = url
+  end
+  
+  
+  def get_response_body
+    uri = URI.parse(url)
+    response = Net::HTTP.get_response_body(uri)
+    response.body
+  end
+
+  def parse_json
+  end
+
+end
